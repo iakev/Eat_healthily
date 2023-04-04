c407facccc5e32049477b4d1d4941e24da7d7d5b
iakev <kirimikmwongera@gmail.com>
2023-04-04 12:17:48 +0300

    added multiple routes for the application to link to various sections

--- web_app/src/router/index.js
+++ web_app/src/router/index.js
@@ -4,6 +4,11 @@ import SignUpView from '../views/SignUpView.vue'
 import LoginView from '../views/LoginView.vue'
 import FarmerPageView from '../views/FarmerPageView.vue'
 import ConsumerPageView from '../views/ConsumerPageView.vue'
+import FarmProductsView from '../views/FarmProductsView.vue'
+// import FarmProductView from '../views/FarmProductView.vue'
+import TrackingView from '../views/TrackingView.vue'
+import FarmPageView from '../views/FarmPageView.vue'
+import InputView from '../views/InputView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,14 +29,39 @@ const router = createRouter({
       component: LoginView
     },
     {
-      path: '/farmerdash',
-      name: 'farmerdash',
+      path: '/farmers/:farmer_id',
+      name: 'farmer',
       component: FarmerPageView
+    },
+    {
+      path: '/farmpage/:farm_id',
+      name: 'farm',
+      component: FarmPageView
     },
     {
       path: '/consumerdash',
       name: 'consumerdash',
       component: ConsumerPageView
+    },
+    {
+      path: '/farmproducts/:product_id',
+      name: 'farmproducts',
+      component: FarmProductsView
+    },
+    // {
+    //   path: '/farmproducts/:farmproduct_id',
+    //   name: 'farmproduct',
+    //   component: FarmProductView
+    // },
+    {
+      path: '/traking/:farmproduct_id',
+      name: 'tracking',
+      component: TrackingView
+    },
+    {
+      path: '/inputs',
+      name: 'inputs',
+      component: InputView
     },
     {
       path: '/about',
