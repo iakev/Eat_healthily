ead51dc5d0332a6169761609cd7b5d244d0aa52d
iakev <kirimikmwongera@gmail.com>
2023-04-05 14:28:13 +0300

    Removing unwanted commented out code

--- api/v1/views/operations.py
+++ api/v1/views/operations.py
@@ -6,23 +6,7 @@ from models.farms import Farm
 from models.products import Produce
 from models.operations import Operation
 
-# @app_views.route('/farms/<farm_id>/operations', methods=['GET'], strict_slashes=False)
-# def get_farm_operations(farm_id):
-#     """Retrieve a list of all operations linked to a specific farm"""
-#     farm = storage.get(Farm, farm_id)
-#     if not farm:
-#         abort(404)
-#     operations = [operation.to_dict() for operation in farm.operations]
-#     return jsonify(operations)
 
-# @app_views.route('/farms/<farm_id>/products/<product_id>/operations', methods=['GET'], strict_slashes=False)
-# def get_product_operations(product_id):
-#     """Retrieves a list of all operations linked to a specific product"""
-#     produce = storage.get(Produce, product_id)
-#     if not produce:
-#         abort(404)
-#     operations = [operation.to_dict() for operation in produce.operations]
-#     return jsonify(operations)
 @app_views.route('/operations', methods=['GET'], strict_slashes=False)
 def get_operations():
     """Retrieves a list of all operations"""
@@ -49,30 +33,6 @@ def post_operations():
     operation.save()
     return make_response(jsonify(operation.to_dict()), 201)
 
-# @app_views.route('farms/<farm_id>/products/<product_id>/operations', methods=['POST'], strict_slashes=False)
-# def post_produce_operations(farm_id,product_id):
-#     """Creates and links operation to a product linked to a farm"""
-#     farm = storage.get(Farm, farm_id)
-#     if not farm:
-#         abort(404)
-#     produce = storage.get(Produce, product_id)
-#     if not produce:
-#         abort(404)
-#     if produce in farm.products:
-#         if not request.get_json():
-#             abort(400, description="Not a valid JSON")
-#         if 'operation_name' not in request.get_json():
-#             abort(400, description="Missing operation name")
-#         if 'description' not in request.get_json():
-#             abort(400, description="Missing description")
-#         data = request.get_json()
-#         operation = Operation(**data)
-#         operation.product_id = produce.id
-#         operation.save()
-#     else:
-#         abort(404)
-#     return make_response(jsonify(operation.to_dict()), 201)
-    
 
 @app_views.route('/operations/<operation_id>', methods=['PUT'], strict_slashes=False)
 def update_operations(operation_id):
