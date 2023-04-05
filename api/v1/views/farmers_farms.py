90d96cf69552101735cbbf264bafe21c7d92b5c0
iakev <kirimikmwongera@gmail.com>
2023-04-05 14:22:57 +0300

    Modified to allow for easy asecure uploading of image files related to farms

--- api/v1/views/farmers_farms.py
+++ api/v1/views/farmers_farms.py
@@ -1,9 +1,26 @@
 """view to create farms and link to farmers"""
 from api.v1.views import app_views
-from flask import abort, jsonify, make_response, request
+from flask import abort,  current_app, jsonify, make_response, request
 from models import storage
 from models.farmers import Farmer
 from models.farms import Farm
+import os
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'pdf'}
+
+
+time_format = "%Y-%m-%d"
+
+def allowed_filename(filename):
+    """validate an extension is valid before upload"""
+    return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def upload_file(file):
+    """Method to upload files and save them in a folder specified"""
+    filename = secure_filename(file.filename)
+    return filename
 
 @app_views.route('/farms', methods=['GET'], strict_slashes=False)
 def get_farms():
@@ -24,19 +41,25 @@ def post_farmer_farm(farmer_id, farm_id=None):
         farm = storage.get(Farm, farm_id)
         if not farm:
             abort(404)
-        farmer.farms.append(farm)
+        if farm not in farmer.farms:
+            farmer.farms.append(farm)
         storage.save()
     else:
-        if request.get_json() is None:
-            abort(400, description="Not a JSON request")
         farmer = storage.get(Farmer, farmer_id)
         if not farmer:
             abort(404)
-        if 'farm_name' not in request.get_json():
+        data = request.form.to_dict()
+        if request.files:
+            if 'image_file' in request.files:
+                image_file = request.files['image_file']
+                if image_file and allowed_filename(image_file.filename):
+                    image_file_name = upload_file(image_file)
+                    image_file.save(os.path.join(current_app.config['UPLOAD_FOLDER'],  image_file_name))
+                    data['image_file'] = '/uploads' + '/' + image_file_name
+        if 'farm_name' not in data:
             abort(400, description="Missing farm name")
-        if 'address' not in request.get_json():
+        if 'address' not in data:
             abort(400, description="Missing address")
-        data = request.get_json()
         data['farmer_id'] = farmer.id
         farm = Farm(**data)
         farm.save()
