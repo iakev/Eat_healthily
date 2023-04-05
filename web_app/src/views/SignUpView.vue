e66279b47170b02dad94e95e08a974ca72123675
iakev <kirimikmwongera@gmail.com>
2023-04-05 14:40:14 +0300

    Modified to allow for consumers, farmers and farms have images associated with them

--- web_app/src/views/SignUpView.vue
+++ web_app/src/views/SignUpView.vue
@@ -34,6 +34,19 @@
                                 <input class="input is-focused" type="password" placeholder="Enter Password" v-model="password">
                             </div>  
                         </div>
+                        <div class="file">
+                            <label class="file-label">
+                                <input class="file-input" type="file" @change="checkFile" name="produceImage">
+                                <span class="file-cta">
+                                    <span class="file-icon"> 
+                                        <i class="fas fa-upload"></i>
+                                    </span>
+                                    <span class="file-label">
+                                        Choose a profile pic...
+                                    </span>
+                                </span> 
+                            </label>
+                        </div>
                         <div class="field-body">
                             <div class="field">
                                 <div class="control">
@@ -59,6 +72,19 @@
                             <div class="control">
                                 <input class="input is-focused" type="text" placeholder="Address" v-model="address">
                             </div>  
+                        </div>
+                        <div class="file">
+                            <label class="file-label">
+                                <input class="file-input" type="file" @change="checkFarm" name="produceImage">
+                                <span class="file-cta">
+                                    <span class="file-icon"> 
+                                        <i class="fas fa-upload"></i>
+                                    </span>
+                                    <span class="file-label">
+                                        Choose a farm pic...
+                                    </span>
+                                </span> 
+                            </label>
                         </div>
                     </div>
                 </div>
@@ -88,20 +114,23 @@ export default{
             address: "",
             consumer: {},
             farmer: {},
-            farm: {}
+            farm: {},
+            file: "",
+            farmImage: ""
         }
     },
     methods:{
         async consumerSignUp() {
-            const data = {
-                first_name: this.firstName,
-                last_name: this.lastName,
-                email: this.email,
-                password: this.password,
-                phone: this.phoneNumber
-            }
+            const headers = { 'Content-Type': 'multipart/form-data' }
+            const consumerData =  new FormData();
+            consumerData.append('first_name', this.firstName);
+            consumerData.append('email', this.email);
+            consumerData.append('password', this.password);
+            consumerData.append('phone', this.phoneNumber);
+            consumerData.append('image_file', this.file);
+
             await axios
-                .post('api/v1/consumers', data)
+                .post('api/v1/consumers', consumerData, { headers })
                 .then(response => {
                     this.consumer = response.data[0];
                     this.$router.push('/login');
@@ -111,19 +140,25 @@ export default{
                 })
         },
         async farmerSignUp() {
-            const farmerData = {
-                first_name: this.firstName,
-                last_name: this.lastName,
-                email: this.email,
-                password: this.password,
-                phone: this.phoneNumber
+            const headers = {
+                'Content-Type': 'multipart/form-data'
             }
-            const farmData = {
-                farm_name: this.farmName,
-                address: this.address
-            }
+
+            const farmerData = new FormData();
+            farmerData.append('first_name', this.firstName);
+            farmerData.append('last_name', this.lastName);
+            farmerData.append('email', this.email);
+            farmerData.append('password', this.password,)
+            farmerData.append('phone', this.phoneNumber)
+            farmerData.append('image_file', this.file);
+
+            const farmData = new FormData();
+            farmData.append('farm_name', this.farmName);
+            farmData.append('address', this.address);
+            farmData.append('image_file', this.farmImage);
+
             await axios
-                .post('api/v1/farmers', farmerData)
+                .post('api/v1/farmers', farmerData, { headers })
                 .then(response => {
                     this.farmer = response.data[0];
                 })
@@ -131,7 +166,7 @@ export default{
                     console.log(error);
                 })
             await axios
-                .post(`api/v1/farmers/${this.farmer.id}/farms`, farmData)
+                .post(`api/v1/farmers/${this.farmer.id}/farms`, farmData, { headers })
                 .then(response => {
                     this.farm = response.data[0];
                     this.$router.push('/login');
@@ -139,6 +174,12 @@ export default{
                 .catch(error => {
                     console.log(error);
                 })
+        },
+        checkFile(event) {
+            this.file = event.target.files[0];
+        },
+        checkFarm(event) {
+            this.farmImage = event.target.files[0];
         }   
     }
 }
