d82318238a0dd43a6c9dda2f8e52e042d498d22a
iakev <kirimikmwongera@gmail.com>
2023-04-05 14:25:58 +0300

    Modified to upload input images correctly and also dealt with toxicity_level enum in a better manner using callables

--- api/v1/views/inputs.py
+++ api/v1/views/inputs.py
@@ -38,34 +38,40 @@ def get_input(input_id):
 @app_views.route('/inputs', methods=['POST'], strict_slashes=False)
 def post_inputs():
     """Posts a new input"""
-    if not request.get_json():
-        abort(400, description="Not a valid JSON")
-    if 'input_name' not in request.get_json():
+    tox_level_dic = {"1" : "Highly toxic", "2" :"Toxic", 
+                     "3": "Moderately toxic", 
+                     "4" :"Slightly toxic","5": "Virtually non-toxic"}
+    data = request.form.to_dict()
+    if 'input_name' not in data:
         abort(400, description="Missing input_name") 
-    if 'source' not in request.get_json():
+    if 'source' not in data:
         abort(400, description="Missing input source")
     if request.files:
         if 'image_file' in request.files:
             image_file = request.files['image_file']
             if image_file and allowed_filename(image_file.filename):
                 image_file_name = upload_file(image_file)
-                image_file.save(os.pth.join(current_app.config['UPLOAD_FOLDER'],  image_file_name))
+                image_file.save(os.path.join(current_app.config['UPLOAD_FOLDER'],  image_file_name))
+                data['image_file'] = '/uploads' + '/' + image_file_name
         if 'label_file' in request.files:
             label_file = request.files['label_file']
             if label_file and allowed_filename(label_file.filename):
                 label_file_name = upload_file(label_file)
-                label_file.save(os.pth.join(current_app.config['UPLOAD_FOLDER'],  label_file_name))
+                label_file.save(os.path.join(current_app.config['UPLOAD_FOLDER'],  label_file_name))
+                data['label_file'] = '/uploads' + '/' + label_file_name
         if 'user_manual_file' in request.files:
             user_manual_file = request.files['user_manual_file']
             if user_manual_file and allowed_filename(user_manual_file.filename):
                 user_manual_file_name = upload_file(user_manual_file)
-                user_manual_file.save(os.pth.join(current_app.config['UPLOAD_FOLDER'],  user_manual_file_name))
-
-    data = request.get_json()
-    data['image_file'] = image_file_name
-    data['label_file'] = label_file_name
-    data['user_manual_file_name'] = user_manual_file_name
+                user_manual_file.save(os.path.join(current_app.config['UPLOAD_FOLDER'],  user_manual_file_name))
+                data['user_manual_file_name'] = '/uploads' + '/' + user_manual_file_name
+    if data['toxicity_level'] in tox_level_dic.keys():
+        data['toxicity_level'] = tox_level_dic[data['toxicity_level']] 
     input = Input(**data)
+    inputs = [value for value in storage.all(Input).values()]
+    for current_input in inputs:
+        if current_input.input_name == input.input_name:
+            return jsonify(current_input.to_dict())
     input.save()
     return make_response(jsonify(input.to_dict(), 201))
     
