c6924fd14989b1d92ad1ffac843e084e73641b2a
iakev <kirimikmwongera@gmail.com>
2023-04-05 14:21:29 +0300

    Modified to allow for easy asecure uploading of image files related to consumers (profile pics)

--- api/v1/views/consumers.py
+++ api/v1/views/consumers.py
@@ -1,17 +1,38 @@
 #!/usr/bin/env python
 """view to create and get the consumers account"""
 from api.v1.views import app_views
-from flask import abort, jsonify, make_response, request
+from flask import abort, current_app, jsonify, make_response, request
 from models.consumers import Consumer
 from models import storage
+import os
+from werkzeug.utils import secure_filename
 
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'pdf'}
+
+
+time_format = "%Y-%m-%d"
+
+def allowed_filename(filename):
+    """validate an extension is valid before upload"""
+    return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def upload_file(file):
+    """Method to upload files and save them in a folder specified"""
+    filename = secure_filename(file.filename)
+    return filename
 
 @app_views.route('/consumers', methods=['POST'], strict_slashes=False)
 def create_consumers():
     """creating a new consumer and saving in the database"""
-    if request.get_json() is None:
-        abort(400, description="Not a Json request")
-    data = request.get_json()
+    data = request.form.to_dict()
+    if request.files:
+        if 'image_file' in request.files:
+            image_file = request.files['image_file']
+            if image_file and allowed_filename(image_file.filename):
+                image_file_name = upload_file(image_file)
+                image_file.save(os.path.join(current_app.config['UPLOAD_FOLDER'],  image_file_name))
+                data['image_file'] = '/uploads' + '/' + image_file_name
     new_consumer = Consumer(**data)
     new_consumer.save()
     return make_response(jsonify(new_consumer.to_dict(), 201))
