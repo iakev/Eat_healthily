5b8c31d086ebf7dba39d431a9d5130f2b7148c0a
iakev <kirimikmwongera@gmail.com>
2023-04-06 07:57:26 +0300

    Modified to add github link to project

--- web_app/src/views/HomeView.vue
+++ web_app/src/views/HomeView.vue
@@ -1,9 +1,104 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <div class="section page-landing">
+      <div class="container">
+        <div class="hero is-info has-text-centered">
+          <div class="hero-body has-text-centered">
+            <p class="title has-text-centered">Eat Healthily</p>
+          </div>
+        </div>
+        <br>
+        <div class="columns is-multiple">
+          <div class="column is-one-fifth">
+            <div class="title">
+              <p class="subtitle"><i>Is healthy really healthy? With all these chemical fertilizers, 
+                pesticides and lack of transparency on farming practices. 
+                It's not easy for consumers to ensure food safety. Trust Eat Healthily to help you track and decide what to consume 
+                and what's not safe to eat. This is achieved by tracking farm produce from planting to harvesting</i></p>
+            </div>
+            <div class="buttons">
+              <RouterLink to="/consumerdash"><button class="button is-info">Track Products</button></RouterLink>
+            </div>
+          </div>
+          <div class="column is-four-fifths">
+            <figure class="image mb-4">
+                <img src=/uploads/Eat_healthy.jpeg alt="" contain height="500px">
+            </figure>
+          </div>
+        </div>
+        <br>
+        <div class="container">
+          <div class="hero is-info">
+            <div class="hero-body has-text-centered"> 
+              <p class="title">Features (what to expect in Eat Healthily platform)</p>
+            </div>
+          </div>
+        </div>
+        <br>
+        <div class="columns is-multiple">
+          <div class="column is-three-quarters"> 
+            <figure class="image mb-4">
+              <img src=/uploads/tracking.jpeg alt="">
+            </figure>
+          </div>
+          <div class="column is-one-quarter">
+            <div class="title">
+              <br>
+              <br>
+              <br>
+              <p class="subtitle"><i>View and track a product evolution from planting to 
+                harvesting. This includes all the operations and inputs to the produce 
+                and if ultimately recommend if the product is safe to consume or not</i></p>
+            </div>
+          </div>
+        </div>
+        <div class="columns is-multiple">
+          <div class="column is-one-quarter">
+            <div class="title">
+              <br>
+              <br>
+              <br>
+              <p class="subtitle"><i>Organise, collect, collate farm and farmer data to be used for quality assurance,
+                management and ultimately marketing purposes since quality is bound to be assured. Hence, ultimately to be used as
+                a farm management tool and also for farmers to track activities carried out and schedule more operations.
+              </i></p>
+            </div>
+          </div>
+          <div class="column is-three-quarters"> 
+            <figure class="image mb-4">
+              <img src=/uploads/smartfarm.jpeg alt="">
+            </figure>
+          </div>
+        </div>
+        <div class="container">
+          <div class="hero is-info">
+            <div class="hero-body has-text-centered"> 
+              <p class="title">About</p>
+            </div>
+            <div class="column is-12">
+              <p class="subtitle">With the rising cases of lifestyle diseases, 
+                unfortunately unscrouplous farmers and business men. The reality is that, 
+                consumers are not sure who to trust with their farm produces. So this project aims at finding a way to ensure
+                safe food consumption. At the same time it attempts to act as a collecting point for all farm realted data. 
+                The importance of collecting this data is for ease of management and ultimately as a marketing tool, since quality is assured.
+                This project is a Portfolio project for <i><a href="https://www.alxafrica.com/software-engineering/">ALX Africa Software</a></i>
+                </p>  
+            </div>
+          </div>
+        </div>
+        <div class="following footer">
+          <div class="content has-text-centered">
+            <p><strong>Follow me on:</strong></p>
+            <a href="https://twitter.com/iakev"><img src=/uploads/icons8-twitter-16.png alt=""></a>
+            <a href="https://github.com/iakev"><img src=/uploads/icons8-github-16.png alt=""></a>
+            <a href="linkedin.com/in/kevin-kirimi-mwongera"><img src=/uploads/icons8-linkedin-2-16.png alt=""></a>
+          </div>
+          <div class="content has-text-centered">
+            <p><strong>This code is hosted at<a href="https://github.com/iakev/Eat_healthily.git">github</a></strong></p>
+          </div>
+        </div>
+        <br>
+      </div>
+    </div>
   </main>
 </template>
